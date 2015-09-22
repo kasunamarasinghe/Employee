06cc1ac242d91a0cc9b0a5cc0e3b7fb3cef5800a
kasunamarasinghe <kasun@davton.com>
2015-09-22 13:34:43 +0530

    Add a bactkto list link

--- Employee/Employee/Models/Emplyee.cs
+++ Employee/Employee/Models/Emplyee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,8 @@ namespace Employee.Models
     [Table("Employee")]
     public class Emplyee
     {
-        public int EID{ get; set; }
+        [Key] 
+        public int EID { get; set; }
         public String EmployeeName{ get; set; }
         public String  Gender{ get; set; }
         public String City{ get; set; }
