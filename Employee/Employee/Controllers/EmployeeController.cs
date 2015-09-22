8338a33cb3f3187efa54c0512f3375de3bc62d13
kasunamarasinghe <kasun@davton.com>
2015-09-22 10:01:20 +0530

    Create the controller, model and view of Employee

---
+++ Employee/Employee/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Employee.Models;
+
+namespace Employee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult EmployeeDetails()
+        {
+            Emplyee employee=new Emplyee()
+            {
+                EID=1,
+
+                EmployeeName="Kasun",
+                Gender="Male",
+                City="Colombo"
+            };
+            return View();
+        }
+
+    }
+}
---
+++ Employee/Employee/Models/Emplyee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee.Models
+{
+    public class Emplyee
+    {
+        public int EID{ get; set; }
+        public String EmployeeName{ get; set; }
+        public String  Gender{ get; set; }
+        public String City{ get; set; }
+    }
+}
